ce77dfd412422e23b21909260f8207a59227749f
Derrick Oswald <derrick.oswald@gmail.com>
2016-01-02 13:52:03 +0100

    more sql

--- src/main/scala/ch/ninecode/CIMRDD.scala
+++ src/main/scala/ch/ninecode/CIMRDD.scala
@@ -4,6 +4,8 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.sql.hive.thriftserver.HiveThriftServer2
 
 //// NOTE: in order to get maven and scala to work together in Eclipse
 //// I needed to install the maven-scala plugin from here:
@@ -37,26 +39,38 @@ object CIMRDD
         {
             if (args.size > 0)
             {
+                // set up the SQL context
+                val sql_context = new HiveContext (spark)
+                println ("context established")
+
+                // start the thrift JDBC server on port 10000
+                HiveThriftServer2.startWithContext (sql_context)
+                println ("thriftserver started")
+
                 // create an RDD by reading in the datafile
                 val rdd = rddFile (spark, args (0))
-                // extract the locations as a new RDD
-                val pf: PartialFunction[(String, ch.ninecode.Element), (String, ch.ninecode.Location)] =
-                {
-                    case x: (String, Element)
-                        if x._2.getClass () == classOf[ch.ninecode.Location] => (x._1, x._2.asInstanceOf[ch.ninecode.Location])
-                }
-                val locations = rdd.collect (pf)
-                locations.persist ()
-                println ("collected " + locations.count () + " locations")
-                // expose the locations as a table
-                val sqlContext = new org.apache.spark.sql.SQLContext (spark)
-                val mydataframe = sqlContext.createDataFrame (locations, classOf [ch.ninecode.Location])
-                mydataframe.registerTempTable ("locations")
-                mydataframe.persist ()
-                println ("locations table created")
+                println ("rdd created")
 
-                // wait for 60 seconds
-                Thread.sleep (60000)
+//                // extract the locations as a new RDD
+//                val pf: PartialFunction[(String, ch.ninecode.Element), (String, ch.ninecode.Location)] =
+//                {
+//                    case x: (String, Element)
+//                        if x._2.getClass () == classOf[ch.ninecode.Location] => (x._1, x._2.asInstanceOf[ch.ninecode.Location])
+//                }
+//                val locations = rdd.collect (pf)
+//                // locations.persist ()
+//                println ("collected " + locations.count () + " locations")
+
+                // create a dataframe from the rdd
+                val mydataframe = sql_context.createDataFrame (rdd, classOf [ch.ninecode.Element])
+                println ("dataframe created")
+
+                // expose the dataframe as a table
+                mydataframe.registerTempTable ("elements")
+                println ("elements table created")
+
+                // wait for 120 seconds
+                Thread.sleep (120000)
 
                 println ("time's up")
             }
