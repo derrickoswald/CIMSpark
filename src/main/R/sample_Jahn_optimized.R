b735688704e9966cd5279aa041c62debb9450fe8
Derrick Oswald <derrick.oswald@gmail.com>
2016-04-14 15:44:38 +0200

    graphx rework

---
+++ src/main/R/sample_Jahn_optimized.R
@@ -0,0 +1,86 @@
+# recorde the load time
+pre = proc.time ()
+
+# set up the Spark system
+Sys.setenv (YARN_CONF_DIR="/home/derrick/spark-1.6.0-bin-hadoop2.6/conf")
+Sys.setenv (SPARK_HOME="/home/derrick/spark-1.6.0-bin-hadoop2.6")
+library (SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
+sc = sparkR.init (sparkJars = c ("/home/derrick/code/CIMScala/target/CIMScala-1.0-SNAPSHOT.jar"), sparkEnvir=list(spark.driver.memory="4g"))
+sqlContext = sparkRSQL.init (sc)
+
+# read the data file and make the edge graph
+elements = sql (sqlContext, "create temporary table elements using ch.ninecode.cim options (path 'hdfs:/user/root/dump_ews.xml')")
+head (sql (sqlContext, "select * from elements"))
+edges = sql (sqlContext, "select * from edges")
+redges = SparkR::as.data.frame (edges)
+
+library (igraph)
+
+# pre = proc.time ()
+
+# keep only non-self connected and non-singly connected edges 
+r2edges = redges[redges$id_seq_1 != redges$id_seq_2 & redges$id_seq_2 != "", ]
+
+# Generate the graph
+graph = graph_from_data_frame (r2edges, directed = F)
+
+# record the start of processing time
+begin = proc.time ()
+
+# Get all the busbars
+kanten = as_data_frame (graph, what = "vertices")
+sammelschienen = kanten[substr (kanten$name, 1, 3)=="SAM",]
+
+# Alle Trafos
+trafos = kanten[substr (kanten$name, 1, 3)=="TRA",]
+
+# data.frame for saving the results
+allbusbars = data.frame (name = character(), distance = character (), stringsAsFactors = FALSE)
+
+
+# for each busbar
+for (sam in sammelschienen)
+{
+    # remember the starting time
+#     start = proc.time ()
+    
+    # convert the sam into a vector, so that the following function can be executed
+    pruefungKnotenIDs = as.vector(sam)
+    
+    # find all vertices reachable from sam 
+    pruefungKnotenliste = subcomponent(graph, pruefungKnotenIDs)
+    
+    # create a subgraph (teilnetzgraph) out of the idenified vertices
+    teilnetzgraph= induced.subgraph(graph, pruefungKnotenliste, impl="auto")
+#     plot(teilnetzgraph)
+
+    # calculate the distance of all vertices to the busbar (this could be the busbar in the station 
+    # or in the distribution box) in the subgraph
+    distMatrix = shortest.paths (teilnetzgraph, v = sam, to = V (teilnetzgraph), weights = E (teilnetzgraph)$length)
+
+    # connect the vertices with the distance-attribute
+    V(teilnetzgraph)$distance = as.numeric (distMatrix[1,])
+    
+    # get a data.frame of all vertices of the subgraph
+    vertices = as_data_frame (teilnetzgraph, what = "vertices")
+
+    # get only the house connections
+    haeuser = vertices[substr (vertices$name, 1, 3) == "HAS",]
+
+    # calculate the mean of all house connections
+    d = mean (haeuser$distance)
+    
+    # add it to the result data.frame
+    busbar = data.frame (name = sam, distance = d, stringsAsFactors = FALSE)
+    allbusbars = rbind (allbusbars, busbar)
+    
+    # show some activity
+#     finish = proc.time ()
+#     print (paste (sam, as.numeric (finish[3] - start[3])))
+}
+
+# output the summary
+end = proc.time ()
+print (paste ("setup", as.numeric (begin[3] - pre[3])))
+print (paste ("total computation", as.numeric (end[3] - begin[3])))
+print ("results in data.frame allbusbars")
--- src/main/scala/ch/ninecode/Sample.scala
+++ src/main/scala/ch/ninecode/Sample.scala
@@ -3,75 +3,152 @@ package ch.ninecode
 import org.apache.spark._
 import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.MutableList
+import org.apache.spark.sql.SQLContext
 
 /**
- *   Execute in the scala-shell with:
- *
- *   import ch.ninecode._
- *   import scala.collection.mutable.HashMap
- *   var options = new HashMap[String, String]
- *   options.put ("path", "file:///opt/data/dump_ews.xml")
- *   val elements = sqlContext.read.format ("ch.ninecode.cim").options (options)
- *   var rdd = elements.load ()
- *   rdd.head ()
- *   var sample = new ch.ninecode.Sample ()
- *   var graph = sample.graphx (sc)
+ *   For an execution example see main().
  *
  */
-
-case class VertexWithDistance (vertex: ch.ninecode.ConnectivityNode, var distance: Double, var abgang: ch.ninecode.Element = null) extends Serializable
 
 class Sample extends Serializable
 {
 
     // based on the Pregel sample at http://spark.apache.org/docs/latest/graphx-programming-guide.html#pregel-api
 
-    def vprog (id: VertexId, v: VertexWithDistance, distance: Double): VertexWithDistance =
+    // messages are a Tuple2 with busbar name String (use hashCode() to get VertexId) and distance as a Double
+
+    var count = 0
+
+    def vprog (id: VertexId, v: (ch.ninecode.ConnectivityNode, (String, Double), String), distances: (String, Double)): (ch.ninecode.ConnectivityNode, (String, Double), String) =
     {
-        if (null != v)
-            v.distance = math.min (v.distance, distance)
-        return (v)
+        var ret = v
+        count += 1
+        if (null == distances)
+        {
+            // initial message
+            val busbar = if ((v._1 != null) && (v._1.name.startsWith ("SAM"))) v._1.name else null
+            if (null != busbar)
+                ret = ((v._1, (busbar, 0.0), v._3))
+        }
+        else
+        {
+            // subsequent messages
+            val abgang = if ((v._1 != null) && v._1.name.startsWith ("ABG") && !v._1.container.startsWith ("_substation")) v._1.name else null
+            ret = ((v._1, (distances._1, distances._2), abgang))
+        }
+        if (null != ret._2._1)
+            println ("vprog " + count + ": " + id + (if (null != ret._1) " " + ret._1.name + "[" + ret._2._1 + "->" + ret._2._2 + "]" else ""))
+        return (ret)
     }
 
-    def sendMessage (triplet: EdgeTriplet[VertexWithDistance, ch.ninecode.cim.Edge]): Iterator[(VertexId, Double)] =
+    def sendMessage (triplet: EdgeTriplet[(ch.ninecode.ConnectivityNode, (String, Double), String), ch.ninecode.cim.Edge]): Iterator[(VertexId, (String, Double))] =
     {
-        var ret:Iterator[(VertexId, Double)] = Iterator.empty
+        var ret:Iterator[(VertexId, (String, Double))] = Iterator.empty
+        if (triplet.attr == null)
+            println ("null attr for " + triplet.toString ())
         if ((null != triplet.srcAttr) && (null != triplet.dstAttr))
         {
-            // identify the upstream abgang vertex - if any
-            val v = triplet.srcAttr.vertex
-            val abgang = if ((null != v) && v.name.startsWith ("ABG") && v.container.startsWith ("_subnetwork")) v else null
-
             // compute the distance to the downstream vertex
-            val distance = triplet.srcAttr.distance + triplet.attr.length
-            if (distance < triplet.dstAttr.distance)
+            val distance = triplet.srcAttr._2._2 + triplet.attr.length
+            // get the distance it may already have
+            val existing = triplet.dstAttr._2._2
+            if (distance < existing)
             {
-                triplet.dstAttr.distance = distance
+                val abgang = triplet.srcAttr._3
                 if (null == abgang)
                 {
-                    println (triplet.srcId + " -> " + (if (triplet.attr != null) (triplet.attr.id_seq_1 + " " + triplet.attr.id_equ + " " + triplet.attr.id_seq_2) else "null") + " -> " + triplet.dstId + " " + distance + "<" + triplet.dstAttr.distance)
-                    ret = Iterator ((triplet.dstId, distance))
+                    println (triplet.srcId + " -> " + (if (triplet.attr != null) (triplet.attr.id_seq_1 + " " + triplet.attr.id_equ + " " + triplet.attr.id_seq_2) else "null") + " -> " + triplet.dstId + " " + distance + "<" + existing)
+                    ret = Iterator ((triplet.dstId, (triplet.srcAttr._2._1, distance)))
                 }
                 else
                 {
-                    triplet.dstAttr.abgang = triplet.srcAttr.abgang
                     // for now just stop
-                    println ("stopping at " + (if (null == triplet.dstAttr.vertex) "**unknown**" else (triplet.dstAttr.vertex.name + distance)))
+                    println ("stopping at " + (if (null == triplet.dstAttr._1) "**unknown**" else triplet.dstAttr._1.name) + " " + (if (triplet.attr != null) (triplet.attr.id_seq_1 + " " + triplet.attr.id_equ + " " + triplet.attr.id_seq_2) else "null") + " " + distance)
                 }
             }
+        }
+        else
+        {
+            if (null == triplet.srcAttr)
+                println ("null srcAttr for " + triplet.toString ())
+            if (null == triplet.dstAttr)
+                println ("null dstAttr for " + triplet.toString ())
         }
         return (ret)
     }
 
-    def mergeMessage (a: Double, b: Double): Double =
+    def mergeMessage (a: (String, Double), b: (String, Double)): (String, Double) =
     {
-        math.min (a, b)
+        if (a._1 != b._1)
+            println ("conflicting Busbars servicing one vertex")
+        return ((a._1, math.min (a._2, b._2)))
     }
 
-    def seqOp (sum: Double, v: Tuple2[VertexId, VertexWithDistance]): Double =
+    def graphx (sc: SparkContext): Graph[(ch.ninecode.ConnectivityNode, (String, Double), String), ch.ninecode.cim.Edge] =
+    {
+        // get the named RDD from the CIM reader
+        var vertices:RDD[ch.ninecode.ConnectivityNode] = null
+        var edges:RDD[ch.ninecode.cim.Edge] = null
+        val rdds = sc.getPersistentRDDs
+        for (key <- rdds.keys)
+        {
+            val rdd = rdds (key)
+            if (rdd.name == "Vertices")
+                vertices = rdds (key).asInstanceOf[RDD[ch.ninecode.ConnectivityNode]]
+            if (rdd.name == "Edges")
+                edges = rdds (key).asInstanceOf[RDD[ch.ninecode.cim.Edge]]
+        }
+
+        // make and process the graph
+        var graph:Graph[(ch.ninecode.ConnectivityNode, (String, Double), String), ch.ninecode.cim.Edge] = null
+        if ((null != vertices) && (null != edges))
+        {
+            println ("Found the vertices RDD: " + vertices.name)
+            println ("Found the edges RDD: " + edges.name)
+
+            // keep only non-self connected and non-singly connected edges
+            edges =  edges.filter ((e: ch.ninecode.cim.Edge) => { (e.id_seq_1 != e.id_seq_2) && e.id_seq_2 != "" })
+
+            // augment the elements to have the distance and upstream abgang using the VertexWithDistance class
+            var elementsplus = vertices.flatMap (
+                (v: ch.ninecode.ConnectivityNode) =>
+                {
+                    Array ((v, (null.asInstanceOf[String], Double.PositiveInfinity), null.asInstanceOf[String]))
+                }
+            )
+
+            // ToDo: what about hashCode() clashes
+            // check: var set = graph.vertices. aggregate (new HashSet[Long]()) ((set, v) => { if (!set.add (v._1)) println (v._1); set }, (set1, set2) => set1++ set2)
+            // with sample data there were none
+
+            // create RDD of (key, value) pairs
+            var _elements = elementsplus.keyBy (_._1.name.hashCode().asInstanceOf[VertexId])
+
+            // convert CIM edges into graphx edges
+            var _edges = edges.flatMap (
+                (e: ch.ninecode.cim.Edge) =>
+                {
+                    Array (new Edge (e.id_seq_1.hashCode(), e.id_seq_2.hashCode(), e))
+                }
+            )
+
+            // construct the graph from the augmented elements (vertices) and edges
+            val default = (null, (null, Double.PositiveInfinity), null)
+            val initial = Graph.apply[(ch.ninecode.ConnectivityNode, (String, Double), String), ch.ninecode.cim.Edge] (_elements, _edges, default)
+
+            // perform the graph tracing
+            graph = initial.pregel (null.asInstanceOf[(String, Double)], Int.MaxValue, EdgeDirection.Either) (vprog, sendMessage, mergeMessage)
+        }
+
+        return (graph)
+    }
+
+    def seqOp (sum: Double, v: Tuple2[VertexId, (ch.ninecode.ConnectivityNode, (String, Double), String)]): Double =
     {
         if (null != v._2)
-            if (v._2.distance != Double.PositiveInfinity) v._2.distance + sum else sum
+            if (v._2._2._2 != Double.PositiveInfinity) v._2._2._2 + sum else sum
         else
             sum
     }
@@ -88,79 +165,33 @@ class Sample extends Serializable
         else
             Double.PositiveInfinity
     }
+}
 
-    def graphx (sc: SparkContext): Graph[VertexWithDistance, ch.ninecode.cim.Edge] =
+object Sample
+{
+    def main (args: Array[String])
     {
-        var vertices:RDD[ch.ninecode.ConnectivityNode] = null
-        var edges:RDD[ch.ninecode.cim.Edge] = null
-        val rdds = sc.getPersistentRDDs
-        for (key <- rdds.keys)
+        // create the configuration
+        val configuration = new SparkConf ()
+        configuration.setAppName ("Simple")
+        configuration.setMaster ("local[2]")
+        // make a Spark context and SQL context
+        val context = new SparkContext (configuration)
+        val sqlContext = new SQLContext (context)
+        context.setLogLevel ("OFF") // Valid log levels include: ALL, DEBUG, ERROR, FATAL, INFO, OFF, TRACE, WARN
+        try
         {
-            val rdd = rdds (key)
-            if (rdd.name == "Vertices")
-                vertices = rdds (key).asInstanceOf[RDD[ch.ninecode.ConnectivityNode]]
-            if (rdd.name == "Edges")
-                edges = rdds (key).asInstanceOf[RDD[ch.ninecode.cim.Edge]]
+             // read the CIM file
+             var options = HashMap[String, String] ("path" -> "file:///home/derrick/code/CIMScala/data/dump_ews.xml")
+             val elements = sqlContext.read.format ("ch.ninecode.cim").options (options)
+             var rdd = elements.load ()
+             rdd.head ()
+             // compute the graph of distances from busbars
+             var sample = new ch.ninecode.Sample ()
+             var graph = sample.graphx (context)
+             // print the sum of the distances
+             println (graph.vertices.aggregate (0.0) (sample.seqOp, sample.combOp))
         }
-
-        var graph:Graph[VertexWithDistance, ch.ninecode.cim.Edge] = null
-        if ((null != vertices) && (null != edges))
-        {
-            println ("Found the vertices RDD: " + vertices.name)
-            println ("Found the edges RDD: " + edges.name)
-
-            // keep only non-self connected and non-singly connected edges
-            edges =  edges.filter ((e: ch.ninecode.cim.Edge) => { (e.id_seq_1 != e.id_seq_2) && e.id_seq_2 != "" })
-
-            // augment the elements to have the distance and upstream abgang using the VertexWithDistance class
-            var elementsplus = vertices.flatMap (
-                (v: ch.ninecode.ConnectivityNode) =>
-                {
-                    Array (VertexWithDistance (v, Double.PositiveInfinity))
-                }
-            )
-
-            // ToDo: what about hashCode() clashes
-            // check: var set = graph.vertices. aggregate (new HashSet[Long]()) ((set, v) => { if (!set.add (v._1)) println (v._1); set }, (set1, set2) => set1++ set2)
-
-            // create RDD of (key, value) pairs
-            var _elements = elementsplus.keyBy (_.vertex.name.hashCode().asInstanceOf[VertexId])
-
-            // convert CIM edges into graphx edges
-            var _edges = edges.flatMap (
-                (e: ch.ninecode.cim.Edge) =>
-                {
-                    Array (new Edge (e.id_seq_1.hashCode(), e.id_seq_2.hashCode(), e))
-                }
-            )
-
-            // construct the graph from the augmented elements (vertices) and edges
-            val default = VertexWithDistance (null, Double.PositiveInfinity)
-            graph = Graph.apply[VertexWithDistance, ch.ninecode.cim.Edge] (_elements, _edges, default)
-
-            // get all the busbars
-            //var sammelschienen = graph.vertices.filter (x => { (x._2 != null) && (x._2.element != null) && (x._2.element.key.startsWith ("_busbar")) })
-
-            // just do one for now
-            var busbar = "SAM1753_node".hashCode ()
-
-            // initialize the graph such that all vertices except the root have distance infinity
-            val initial = graph.mapVertices (
-                (id, v) =>
-                {
-                    if (null != v)
-                        v.distance = if (id == busbar) 0.0 else Double.PositiveInfinity
-                    v
-                }
-            )
-
-            // perform the graph tracing
-            graph = initial.pregel (Double.PositiveInfinity) (vprog, sendMessage, mergeMessage)
-
-            // print the sum of the distances
-            println (graph.vertices.aggregate (0.0) (seqOp, combOp))
-        }
-
-        return (graph)
+        finally context.stop () // clean up
     }
 }
