6534b439b94a289dbdb1186241d81b76d411324a
Derrick Oswald <derrick.oswald@9code.ch>
2019-03-27 14:34:55 +0100

    change SparkSuite.scala from softlink to real file

--- CIMExport/src/test/scala/ch/ninecode/SparkSuite.scala
+++ CIMExport/src/test/scala/ch/ninecode/SparkSuite.scala
@@ -1,1 +1,246 @@
-../../../../../../CIMReader/src/test/scala/ch/ninecode/SparkSuite.scala
+package ch.ninecode
+
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.net.Inet4Address
+import java.net.InetAddress
+import java.net.NetworkInterface
+import java.util
+import java.util.zip.ZipInputStream
+
+import scala.collection.JavaConverters._
+import scala.reflect.ClassTag
+import scala.reflect.classTag
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.SparkSession
+import org.scalatest.Outcome
+import org.scalatest.fixture
+import ch.ninecode.cim.CIMClasses
+
+class SparkSuite extends fixture.FunSuite
+{
+    type FixtureParam = SparkSession
+
+    /**
+     * This utility extracts files and directories of a standard zip file to
+     * a destination directory.
+     *
+     * @author www.codejava.net
+     *
+     */
+    class Unzip
+    {
+        /**
+         * Extracts a zip file specified by the file to a directory.
+         *
+         * The directory will be created if does not exist.
+         *
+         * @param file The Zip file.
+         * @param directory The directory to extract it to
+         * @throws IOException If there is a problem with the zip extraction
+         */
+        @throws[IOException]
+        def unzip (file: String, directory: String): Unit =
+        {
+            val dir = new File (directory)
+            if (!dir.exists)
+                dir.mkdir
+            val zip = new ZipInputStream (new FileInputStream (file))
+            var entry = zip.getNextEntry
+            // iterates over entries in the zip file
+            while (null != entry)
+            {
+                val path = directory + entry.getName
+                if (!entry.isDirectory)
+                    // if the entry is a file, extracts it
+                    extractFile (zip, path)
+                else
+                    // if the entry is a directory, make the directory
+                    new File (path).mkdir
+                zip.closeEntry ()
+                entry = zip.getNextEntry
+            }
+            zip.close ()
+        }
+
+        /**
+         * Extracts a zip entry (file entry).
+         *
+         * @param zip The Zip input stream for the file.
+         * @param path The path to extract he file to.
+         * @throws IOException If there is a problem with the zip extraction
+         */
+        @throws[IOException]
+        private def extractFile (zip: ZipInputStream, path: String): Unit =
+        {
+            val bos = new BufferedOutputStream (new FileOutputStream (path))
+            val bytesIn = new Array[Byte](4096)
+            var read = -1
+            while ({ read = zip.read (bytesIn); read != -1 })
+                bos.write (bytesIn, 0, read)
+            bos.close ()
+        }
+    }
+
+    /**
+     * Delete files and directories recursively.
+     *
+     * @param path The starting path.
+     */
+    def deleteRecursive (path: File): Unit =
+    {
+        if (path.isDirectory)
+            for (subpath <- path.list)
+                deleteRecursive (new File (path, subpath))
+        path.delete
+    }
+
+    /**
+     * Add to the process environment.
+     *
+     * @see https://stackoverflow.com/questions/318239/how-do-i-set-environment-variables-from-java
+     * @param newenv The list of key value pairs to add.
+     */
+    protected def setEnv (newenv: java.util.HashMap[String, String]): Unit =
+    {
+        try
+        {
+            val env: util.Map[String, String] = System.getenv
+            for (cl <- Class.forName ("java.util.Collections").getDeclaredClasses)
+            {
+                if ("java.util.Collections$UnmodifiableMap" == cl.getName)
+                {
+                    val field = cl.getDeclaredField ("m")
+                    field.setAccessible (true)
+                    val obj = field.get (env)
+                    val map = obj.asInstanceOf[java.util.Map[String, String]]
+                    map.putAll (newenv)
+                }
+            }
+        }
+        catch
+        {
+            case e: Exception =>
+                e.printStackTrace ()
+        }
+    }
+
+    /**
+     * Set SPARK_LOCAL_IP to the IP address in dotted-quad format (e.g. 1.2.3.4) if it isn't set.
+     *
+     * Avoids "Set SPARK_LOCAL_IP if you need to bind to another address" warning message.
+     * @see findLocalInetAddress: https://github.com/apache/spark/blob/master/core/src/main/scala/org/apache/spark/util/Utils.scala
+     */
+    def setLocalIP (): Unit =
+    {
+        if (null == System.getenv ("SPARK_LOCAL_IP"))
+        {
+            val address = InetAddress.getLocalHost
+            if (address.isLoopbackAddress)
+            {
+                // Address resolves to something like 127.0.1.1, which happens on Debian; try to find
+                // a better address using the local network interfaces
+                // getNetworkInterfaces returns ifs in reverse order compared to ifconfig output order
+                // on unix-like system. On windows, it returns in index order.
+                // It's more proper to pick ip address following system output order.
+                val interfaces = NetworkInterface.getNetworkInterfaces.asScala.toSeq
+                val windows = System.getProperty ("os.name").startsWith ("Windows") // underlying operating system is Windows
+
+                for (ni <- if (windows) interfaces else interfaces.reverse)
+                {
+                    val addresses = ni.getInetAddresses.asScala.filterNot (addr => addr.isLinkLocalAddress || addr.isLoopbackAddress).toSeq
+                    if (addresses.nonEmpty)
+                    {
+                        val addr = addresses.find (_.isInstanceOf [Inet4Address]).getOrElse (addresses.head)
+                        // because of Inet6Address.toHostName may add interface at the end if it knows about it
+                        val ip = InetAddress.getByAddress (addr.getAddress)
+                        // We've found an address that looks reasonable!
+                        val newenv = new java.util.HashMap[String, String] ()
+                        newenv.put ("SPARK_LOCAL_IP", ip.getHostAddress)
+                        setEnv (newenv)
+                        return
+                    }
+                }
+            }
+        }
+    }
+
+    def withFixture (test: OneArgTest): Outcome =
+    {
+        // set the env to add the real local address
+        setLocalIP ()
+
+        // create the configuration
+        val configuration = new SparkConf (false)
+        configuration.setAppName ("CIMSparkSuite")
+        configuration.setMaster ("local[2]")
+        configuration.set ("spark.driver.memory", "1g")
+        configuration.set ("spark.executor.memory", "1g")
+        configuration.set ("spark.ui.port", "4041")
+        configuration.set ("spark.ui.showConsoleProgress", "false")
+        configuration.registerKryoClasses (CIMClasses.list)
+
+        val session = SparkSession.builder ().config (configuration).getOrCreate () // create the fixture
+        session.sparkContext.setLogLevel ("ERROR") // Valid log levels include: ALL, DEBUG, ERROR, FATAL, INFO, OFF, TRACE, WARN
+        try
+            withFixture (test.toNoArgTest (session)) // "loan" the fixture to the test
+        finally session.stop() // clean up the fixture
+    }
+
+    def readFile (filename: String, options: util.Map[String, String] = new util.HashMap[String, String] ())(implicit spark: SparkSession): DataFrame =
+    {
+        options.put ("StorageLevel", "MEMORY_AND_DISK_SER")
+        spark.read.format ("ch.ninecode.cim").options (options).load (filename) // ToDo: why doesn't this work? load (filename.split (","):_*)
+    }
+
+    /**
+     * Get the named RDD.
+     *
+     * @param name The name of the RDD, usually the same as the CIM class.
+     * @param spark The Spark session which persisted the named RDD.
+     * @tparam T The type of objects contained in the named RDD.
+     * @return The typed RDD, e.g. <code>RDD[T]</code>.
+     *
+     * @example The RDD of all elements is somewhat special,
+     * currently it is named Elements (plural), so this method must be used:
+     * {{{val elements: RDD[Element] = get[Element]("Elements")}}}.
+     *
+     */
+    def get[T : ClassTag](name: String)(implicit spark: SparkSession): RDD[T] =
+    {
+
+        val rdd: collection.Map[Int, RDD[_]] = spark.sparkContext.getPersistentRDDs
+        rdd.find (_._2.name == name) match
+        {
+            case Some ((_, rdd: RDD[_])) =>
+                rdd.asInstanceOf[RDD[T]]
+            case Some (_) =>
+                null
+            case None =>
+                null
+        }
+    }
+
+
+    /**
+     * Get the typed RDD.
+     *
+     * Convenience method where the name of the RDD is the same as the contained
+     * class type (the usual case).
+     *
+     * @param spark The Spark session which persisted the typed RDD.
+     * @tparam T The type of the RDD, e.g. <code>RDD[T]</code>.
+     * @return The RDD with the given type of objects, e.g. <code>RDD[ACLineSegment]</code>.
+     */
+    def get[T : ClassTag](implicit spark: SparkSession): RDD[T] =
+    {
+        val classname = classTag[T].runtimeClass.getName
+        val name = classname.substring (classname.lastIndexOf (".") + 1)
+        get (name)
+    }
+}
